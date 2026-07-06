e241bbc57ee1f3a7fea60d1ec483d48a5ac39f8c
Kimikhwan <moonfgg@naver.com>
2026-07-06 16:49:52 +0900

    Init Unity

---
+++ Assets/02_Scripts/Common/DataTable.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class DataTable
+{
+   
+}
---
+++ Assets/02_Scripts/Common/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Resources;
+using UnityEngine;
+
+public class GameManager : SingletonBehaviour<GameManager>
+{
+    public static ResourceManager Resource { get { return Instance._resourceManager; } }
+    public static SoundManager Sound { get { return Instance._soundManager; } }
+    public static PoolManager Pool { get { return Instance._poolManager; } }
+    public static DataTable DataTable { get { return Instance._dataTable; } }
+    public static SaveManager Save { get { return Instance._userDataManager; } }
+    public static TimeManager Time { get { return Instance._timeManager; } }
+
+    #region Manager Varialbes
+
+    private ResourceManager _resourceManager = new();
+    private SoundManager _soundManager = new();
+    private PoolManager _poolManager = new();
+    private DataTable _dataTable = new();
+    private SaveManager _userDataManager = new();
+    private TimeManager _timeManager = new();
+
+    #endregion
+
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+
+}
