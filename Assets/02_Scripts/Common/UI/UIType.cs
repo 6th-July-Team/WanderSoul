86a07b2b182ea34786ad5b823024cf88818c8f56
이태영 <scammerlee1943@gmail.com>
2026-07-08 12:45:15 +0900

    feat: UIType/UIRootType enum 추가

---
+++ Assets/02_Scripts/Common/UI/UIType.cs
@@ -0,0 +1,22 @@
+﻿public enum UIType
+{
+    TitleUI,
+    InventoryUI,
+    PetInfoUI,
+    TopUI,
+    MainUI,
+    PlayerHudUI,
+    QuestHudUI,
+    PopupUI,
+    MapUI,
+    DialogueUI
+}
+
+public enum UIRootType
+{
+    Background,
+    Main,
+    Content,
+    Popup,
+    Front
+}
