0546c37be533a7adae7f228531a253000f1af894
이태영 <scammerlee1943@gmail.com>
2026-07-08 13:05:40 +0900

    feat: UIManager 기본 구조

---
+++ Assets/02_Scripts/Common/UI/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : SingletonBehaviour<UIManager>
+{
+    [SerializeField] private Canvas _backgroundCanvas;
+    [SerializeField] private Canvas _mainCanvas;
+    [SerializeField] private Canvas _contentCanvas;
+    [SerializeField] private Canvas _popupCanvas;
+    [SerializeField] private Canvas _frontCanvas;
+
+    private Dictionary<UIType, BaseUI> _createdUIDic = new Dictionary<UIType, BaseUI>();
+
+    private HashSet<UIType> _activeUI = new HashSet<UIType>();
+
+    protected override void Init()
+    {
+        base.Init();
+    }
+}
