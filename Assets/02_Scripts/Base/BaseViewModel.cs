5eab4ceefb78d43df8a9626a207a7ca962e88da8
이기웅 <littlemanner1@gmail.com>
2026-07-09 10:34:20 +0900

    feat: IDisposable을 상속받는 BaseViewModel<T>

---
+++ Assets/02_Scripts/Base/BaseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+
+public abstract class BaseViewModel : IDisposable
+{
+    public event Action<string> OnPropertyChanged_View;
+
+    protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEvent)
+    {
+        OnPropertyChanged_View?.Invoke(propertyChangedEvent.PropertyName);
+    }
+
+    public abstract void PropertyChangedOnInit();
+
+    public abstract void Dispose();
+}
+
+public abstract class BaseViewModel<T> : BaseViewModel where T: BaseModel
+{
+    protected readonly T _model;
+
+    public BaseViewModel(T model)
+    {
+        _model = model;
+        _model.PropertyChanged += OnPropertyChanged;
+    }
+
+    public override void Dispose()
+    {
+        _model.PropertyChanged -= OnPropertyChanged;
+    }
+
+    public override void PropertyChangedOnInit()
+    {
+        _model.PropertyChangedOnInit();
+    }
+}
