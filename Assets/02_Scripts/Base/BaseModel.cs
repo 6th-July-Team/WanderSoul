562ff73b4ab3767cf0da09372db631f75238797e
이기웅 <littlemanner1@gmail.com>
2026-07-09 10:33:28 +0900

    feat: INotifyPropertyChanged를 상속받는 BaseModel

---
+++ Assets/02_Scripts/Base/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+public abstract class BaseModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public abstract void PropertyChangedOnInit();
+}
