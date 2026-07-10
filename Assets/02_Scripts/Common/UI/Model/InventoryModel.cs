fa32480877d8dfeddf5d835889eae018107a88ba
이태영 <scammerlee1943@gmail.com>
2026-07-10 17:31:01 +0900

    feat: 인벤토리 데이터 모델 추가 (ItemSlotModel/InventoryModel)

---
+++ Assets/02_Scripts/Common/UI/Model/InventoryModel.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+public class InventoryModel : BaseModel
+{
+    private List<ItemSlotModel> _itemList = new List<ItemSlotModel>();
+
+    private Dictionary<long, ItemSlotModel> _itemDic = new Dictionary<long, ItemSlotModel>();
+
+    public List<ItemSlotModel> ItemList { get { return _itemList; } }
+    public void AddItem(ItemSlotModel item)
+    {
+        if (item == null)
+        {
+            return;
+        }
+        _itemList.Add(item);
+        _itemDic[item.ItemUniqueId] = item;
+        OnPropertyChanged(nameof(ItemList));
+    }
+
+    public void RemoveItem(long ItemUniqueId)
+    {
+        if (_itemDic.ContainsKey(ItemUniqueId) == false)
+        {
+            return;
+        }
+
+        OnPropertyChanged(nameof(ItemList));
+        var item = _itemDic[ItemUniqueId];
+        _itemList.Remove(item);
+        _itemDic.Remove(ItemUniqueId);
+        OnPropertyChanged(nameof(ItemList));
+    }
+
+    public ItemSlotModel GetItem(long itemUniqueId)
+    {
+        if (_itemDic.ContainsKey(itemUniqueId) == false)
+        {
+            return null;
+        }
+        return _itemDic[itemUniqueId];
+    }
+
+
+
+    public override void PropertyChangedOnInit()
+    {
+        OnPropertyChanged(nameof(ItemList));
+    }
+
+
+}
