5036f6f73311705eb4f301c2b4f25007d658768f
이태영 <scammerlee1943@gmail.com>
2026-07-11 18:04:20 +0900

    feat: 인벤토리 View(아이템 슬롯)와 ViewModel 구현

---
+++ Assets/02_Scripts/Common/UI/ViewModel/InventoryViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class InventoryViewModel : BaseViewModel<InventoryModel>
+{
+    public InventoryViewModel(InventoryModel model) : base(model)
+    {
+    }
+
+    public List<ItemSlotModel> ItemList
+    {
+        get { return _model.ItemList; }
+    }
+
+    public void AddItem(ItemSlotModel item)
+    {
+        _model.AddItem(item);
+    }
+
+    public void RemoveItem(long itemUniqueId)
+    {
+        _model.RemoveItem(itemUniqueId);
+    }
+
+    public ItemSlotModel GetItem(long itemUniqueId)
+    {
+        return _model.GetItem(itemUniqueId);
+    }
+}
