bec027e324e10b59baab219ac85f2781bc363661
신요한 <3338775@gmail.com>
2026-07-14 15:29:36 +0900

    feat: 마을 시설 클릭 및 상점 간단 UI 구현

---
+++ Assets/02_Scripts/Village/ShopItemCard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ShopItemCard : MonoBehaviour
+{
+    [SerializeField] private string _itemName;
+    [SerializeField] private int _itemPrice;
+
+    public void Buy()
+    {
+        Debug.Log($"Bought {_itemName} for {_itemPrice} gold.");
+    }
+}
---
+++ Assets/02_Scripts/Village/VillageClickInput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class VillageClickInput : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _shopPanel;
+
+    private void Awake()
+    {
+        _shopPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            return;
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return;
+        }
+
+        if (!hit.collider.TryGetComponent(out VillageFacility facility))
+        {
+            return;
+        }
+
+        if (facility.FacilityType == VillageFacilityType.Shop)
+        {
+            _shopPanel.SetActive(true);
+        }
+
+        if (facility.FacilityType == VillageFacilityType.MissionGuild)
+        {
+            Debug.Log("Mission Guild clicked");
+        }
+    }
+}
