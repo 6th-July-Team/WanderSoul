545d4699807596fff75f05700cc1c57d86d6ae17
신요한 <3338775@gmail.com>
2026-07-15 21:41:12 +0900

    feat: 상점, 장비 상점 탭 전환  UI 구조 구현

---
+++ Assets/02_Scripts/Village/Shoppanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoppanel : MonoBehaviour
+{
+    [SerializeField] private Button _generalShopButton;
+    [SerializeField] private Button _equipmentShopButton;
+
+    [SerializeField] private GameObject _itemGrid;
+    [SerializeField] private GameObject _equipmentItemGrid;
+
+    private void OnEnable()
+    {
+        showGeneralShop();
+    }
+
+    public void showGeneralShop()
+    {
+        _itemGrid.SetActive(true);
+        _equipmentItemGrid.SetActive(false);
+
+        _generalShopButton.image.color = Color.white;
+        _equipmentShopButton.image.color = Color.gray;
+    }
+
+    public void showEquipmentShop()
+    {
+        _itemGrid.SetActive(false);
+        _equipmentItemGrid.SetActive(true);
+
+        _generalShopButton.image.color= Color.gray;
+        _equipmentShopButton.image.color = Color.white;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
