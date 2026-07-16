0598e5391fd16a8d5e3197f496fd9d3084dd65fd
이태영 <scammerlee1943@gmail.com>
2026-07-16 17:57:18 +0900

    fix: 실수 수정

--- Assets/02_Scripts/Data/Data.cs
+++ Assets/02_Scripts/Data/Data.cs
@@ -247,24 +247,24 @@ public class PetStatData : BaseData
             _ => 0f
         };
     }
+}
 
-    [Serializable]
-    public class LevelUpOptionData : BaseData
-    {
-        public string Name;
-        public string Description;
-        public string OptionCategory;
-        public string Grade;
-        public int MinLevel;
-        public int MaxLevel;
-        public string RequiredClassId;
-        public string TargetStatType;
-        public string TargetSkillSlot;
-        public string EffectType;
-        public string Operation;
-        public float Value;
-        public int MaxStack;
-        public int Weight;
-        public string IconPath;
-    }
+[Serializable]
+public class LevelUpOptionData : BaseData
+{
+    public string Name;
+    public string Description;
+    public string OptionCategory;
+    public string Grade;
+    public int MinLevel;
+    public int MaxLevel;
+    public string RequiredClassId;
+    public string TargetStatType;
+    public string TargetSkillSlot;
+    public string EffectType;
+    public string Operation;
+    public float Value;
+    public int MaxStack;
+    public int Weight;
+    public string IconPath;
 }
