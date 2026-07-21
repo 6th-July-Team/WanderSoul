8ddc8c014bbbd429cd13859d6a7510c8afa1e0d5
Kimikhwan <moonfgg@naver.com>
2026-07-21 16:30:59 +0900

    feat: 자료구조형 MVVM 시도

---
+++ Assets/02_Scripts/Player/MVVM/PlayerSkillViewModel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerSkillViewModel : BaseViewModel<PlayerSkillModel>
+{
+    PlayerSkillModel model;
+
+    public PlayerSkillViewModel(PlayerSkillModel model) : base(model)
+    {
+        this.model = model;
+    }
+
+    public void UseSkill(SkillSlot slot)
+    {
+        model.SkillCoolTimes[slot] = 0;
+    }
+
+    public float GetSkillCoolTime(SkillSlot slot)
+    {
+        return model.SkillCoolTimes[slot];
+    }
+
+    public void Update(float deltaTime)
+    {
+
+    }
+}
