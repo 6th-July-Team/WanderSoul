48403647c2b3b88626f6fb40c6fa586713e5ad23
이기웅 <littlemanner1@gmail.com>
2026-07-23 02:21:56 +0900

    test

---
+++ Assets/02_Scripts/Projectile/Test/ProjectileTestObject.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public enum ProjectileType
+{
+    ShcolarFireProjectile,
+
+}
+
+
+public class ProjectileTestObject : MonoBehaviour
+{
+    [SerializeField] private GameObject Prefab_Projectile;
+    [SerializeField] private Transform Transform_ShootPosition;
+
+    [SerializeField] private int ProjectileSpeed;
+    [SerializeField] private float ProjectileDamage;
+    [SerializeField] private float ProjectileAdditionalDamage;
+    [SerializeField] private float ProjectileBoomRange;
+    [SerializeField] private float ProjectilePierce;
+
+    [SerializeField] private DamageType ProjectileDamageType;
+    [SerializeField] private TargetType TragetType;
+    [SerializeField] private ProjectileType ProjectileType;
+
+    [SerializeField] private float FireSecond;
+    private float _time = 0;
+    
+    [SerializeField] private bool IsCreateProjectile;
+
+
+    private void Awake()
+    {
+    }
+
+    private void Update()
+    {
+        if (IsCreateProjectile)
+        {
+            _time += Time.deltaTime;
+
+            if (_time >= FireSecond)
+            {
+                CreateProjectileAndFire();
+                _time = 0;
+            }
+        }
+    }
+
+    private void CreateProjectileAndFire()
+    {
+        GameObject projectile = Instantiate(Prefab_Projectile, Transform_ShootPosition.position, Quaternion.identity);
+
+        ShcolarFireProjectile projectileComponent = projectile.GetComponent<ShcolarFireProjectile>();
+
+        if (projectileComponent == null)
+        {
+            Logger.LogError("생성한 Proejctile에 ShcolarFireProjectile가 없습니다.");
+        }
+
+        ProjectileStruct data = new ProjectileStruct(ProjectileSpeed, ProjectileDamage, Vector3.forward, ProjectileDamageType, TragetType, ProjectileAdditionalDamage, radius: ProjectileBoomRange, pierce: ProjectilePierce);
+        projectileComponent.Init(data);
+    }
+}
