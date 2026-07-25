09f785d85585fc01e1f14c09b68990908f43c297
이태영 <scammerlee1943@gmail.com>
2026-07-25 23:26:58 +0900

    feat: 버튼 호버/클릭 애니메이션 추가

--- Assets/02_Scripts/Common/UI/UIButton.cs
+++ Assets/02_Scripts/Common/UI/UIButton.cs
@@ -1,21 +1,55 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIButton : MonoBehaviour, IPointerClickHandler
+    , IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private Button _button;
     [SerializeField] private TMP_Text _text;
 
+    [Header("Animation")]
+    [SerializeField] private RectTransform _scaleTarget;
+    [SerializeField] private float _hoverScale = 1.08f;
+    [SerializeField] private float _pressScale = 0.92f;
+    [SerializeField] private float _duration = 0.12f;
+
+    private Vector3 _originScale = Vector3.one;
+    private bool _isHovering = false;
+
     private void Awake()
     {
         InitButton();
+        InitScaleTarget();
+    }
+
+    private void OnEnable()
+    {
+        _isHovering = false;
+        ResetScale();
+    }
+
+    private void InitScaleTarget()
+    {
+        if (_scaleTarget == null)
+        {
+            _scaleTarget = transform as RectTransform;
+        }
+
+        if (_scaleTarget != null)
+        {
+            _originScale = _scaleTarget.localScale;
+        }
     }
 
     private void OnDisable()
     {
+        _isHovering = false;
+        ResetScale();
+
         //if (_button != null)
         //{
         //    _button.onClick.RemoveAllListeners();
@@ -78,6 +112,88 @@ public class UIButton : MonoBehaviour, IPointerClickHandler
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        // TODO(태영,07-08): 클릭 애니메이션/사운드 추가 예정
+        // TODO(태영,07-08): 클릭 사운드 추가 예정
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (IsAnimatable() == false)
+        {
+            return;
+        }
+
+        _isHovering = true;
+        ScaleTo(_hoverScale, Ease.OutQuad);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (IsAnimatable() == false)
+        {
+            return;
+        }
+
+        _isHovering = false;
+        ScaleTo(1f, Ease.OutQuad);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (IsAnimatable() == false)
+        {
+            return;
+        }
+
+        ScaleTo(_pressScale, Ease.OutQuad);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (IsAnimatable() == false)
+        {
+            return;
+        }
+
+        float target = 1f;
+
+        if (_isHovering == true)
+        {
+            target = _hoverScale;
+        }
+
+        ScaleTo(target, Ease.OutBack);
+    }
+
+    private bool IsAnimatable()
+    {
+        if (_scaleTarget == null)
+        {
+            return false;
+        }
+
+        if (_button != null && _button.interactable == false)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void ScaleTo(float ratio, Ease ease)
+    {
+        _scaleTarget.DOKill();
+
+        _scaleTarget.DOScale(_originScale * ratio, _duration).SetEase(ease);
+    }
+
+    private void ResetScale()
+    {
+        if (_scaleTarget == null)
+        {
+            return;
+        }
+
+        _scaleTarget.DOKill();
+        _scaleTarget.localScale = _originScale;
     }
 }
